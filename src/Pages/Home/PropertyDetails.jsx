4094b93f756a1a233f3ad1ccef61b73d4c737bab
unknown <mosarofhossen5228@gmail.com>
2024-08-29 10:48:20 +0600

    viewDetails route not completed

---
+++ src/Pages/Home/PropertyDetails.jsx
@@ -0,0 +1,9 @@
+const PropertyDetails = () => {
+  return (
+    <div>
+      <h2>this is property details</h2>
+    </div>
+  );
+};
+
+export default PropertyDetails;
--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -3,6 +3,7 @@ import Root from "../Layout/Root";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Registration from "../Pages/Registration/Registration";
+import PropertyDetails from "../Pages/Home/PropertyDetails";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,12 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
         loader: () => fetch("properties.json"),
+      },
+      {
+        path: "/propertyDetails/:id",
+        element: <PropertyDetails></PropertyDetails>,
+
+        // loader:({params})=>{``}
       },
       {
         path: "/login",
